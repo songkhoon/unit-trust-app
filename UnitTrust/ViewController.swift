6b5dc62e5aebe65ad9c0fdf624ae677507a95ef2
Jeff <jeff.lim@compass-interactive.com>
2018-03-01 08:11:17 +0800

    Initial Commit

---
+++ UnitTrust/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UnitTrust
+//
+//  Created by Jeff on 01/03/2018.
+//  Copyright © 2018 Jeff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
