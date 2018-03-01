46599b6443219698ad8091de59d1be78bf6525d5
Jeff <jeff.lim@compass-interactive.com>
2018-03-01 08:15:49 +0800

    pod init

---
+++ Pods/Target Support Files/Pods-UnitTrust/Pods-UnitTrust-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UnitTrust : NSObject
+@end
+@implementation PodsDummy_Pods_UnitTrust
+@end
---
+++ Pods/Target Support Files/Pods-UnitTrustTests/Pods-UnitTrustTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UnitTrustTests : NSObject
+@end
+@implementation PodsDummy_Pods_UnitTrustTests
+@end
